ff66de11251feced2130d47e18ef47164b056b5b
Viktor Balog <viktor.istvan.balogh@gmail.com>
2020-01-04 15:31:00 +0100

    fixed some issues made hold_until optional

--- src/components/NestThermostat.vue
+++ src/components/NestThermostat.vue
@@ -41,6 +41,7 @@
         </horseshoe-slider>
         <horseshoe-slider
           id="timeChangeSlider"
+          v-if="hasHoldUntil"
           :active-tick-color="activeTickColor"
           :inactive-tick-color="inactiveTickColor"
           :background-color="timeBackgroundColor"
@@ -77,30 +78,31 @@
             <rect
               :width="diameter * 0.12"
               :height="diameter * 0.14"
-              :x="iconOffsetX()"
-              :y="iconOffsetY()"
+              :x="iconOffset.x"
+              :y="iconOffset.y"
               fill="transparent"
             />
             <path
               :fill="stateIconColor"
-              :transform="`translate(${iconOffsetX()} ${iconOffsetY()}) scale(2)`"
+              :transform="`translate(${iconOffset.x} ${iconOffset.y}) scale(2)`"
               d="M17.55,11.2C17.32,10.9 17.05,10.64 16.79,10.38C16.14,9.78 15.39,9.35 14.76,8.72C13.3,7.26 13,4.85 13.91,3C13,3.23 12.16,3.75 11.46,4.32C8.92,6.4 7.92,10.07 9.12,13.22C9.16,13.32 9.2,13.42 9.2,13.55C9.2,13.77 9.05,13.97 8.85,14.05C8.63,14.15 8.39,14.09 8.21,13.93C8.15,13.88 8.11,13.83 8.06,13.76C6.96,12.33 6.78,10.28 7.53,8.64C5.89,10 5,12.3 5.14,14.47C5.18,14.97 5.24,15.47 5.41,15.97C5.55,16.57 5.81,17.17 6.13,17.7C7.17,19.43 9,20.67 10.97,20.92C13.07,21.19 15.32,20.8 16.93,19.32C18.73,17.66 19.38,15 18.43,12.72L18.3,12.46C18.1,12 17.83,11.59 17.5,11.21L17.55,11.2M14.45,17.5C14.17,17.74 13.72,18 13.37,18.1C12.27,18.5 11.17,17.94 10.5,17.28C11.69,17 12.39,16.12 12.59,15.23C12.76,14.43 12.45,13.77 12.32,13C12.2,12.26 12.22,11.63 12.5,10.94C12.67,11.32 12.87,11.7 13.1,12C13.86,13 15.05,13.44 15.3,14.8C15.34,14.94 15.36,15.08 15.36,15.23C15.39,16.05 15.04,16.95 14.44,17.5H14.45Z"
             />
         </g>
         <g
+          v-if="hasHoldUntil"
           @click="toggleMode"
           style="cursor: pointer"
         >
           <rect
             :width="diameter * 0.12"
             :height="diameter * 0.14"
-            :x="iconOffsetX(0.15)"
-            :y="iconOffsetY()"
+            :x="iconOffset.offsetX(0.15)"
+            :y="iconOffset.y"
             fill="transparent"
           />
           <path
             :fill="modeIconColor"
-            :transform="`translate(${iconOffsetX(0.15)} ${iconOffsetY()}) scale(2)`"
+            :transform="`translate(${iconOffset.offsetX(0.15)} ${iconOffset.y}) scale(2)`"
             d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M16.24,7.76C15.07,6.58 13.53,6 12,6V12L7.76,16.24C10.1,18.58 13.9,18.58 16.24,16.24C18.59,13.9 18.59,10.1 16.24,7.76Z"
           />
         </g>
@@ -173,7 +175,7 @@ export default {
     },
     holdUntil: {
       type: String,
-      default: '2019-12-31T23:55:00+01:00'
+      default: ''
     },
     presetMode: {
       type: String,
@@ -210,7 +212,7 @@ export default {
       requestedMode: 'off',
       requestedTime: 0,
       offsetMinutes: -1,
-      mode: 'time',
+      mode: 'temp',
       dragging: false
     }
   },
@@ -221,7 +223,9 @@ export default {
     var holdUntil = moment(this.holdUntil)
     var now = moment()
 
-    this.requestedTime = this.roundHalf(holdUntil.diff(now, 'minutes') / 60) * 2
+    if(this.isHoldUntil) {
+      this.requestedTime = this.roundHalf(holdUntil.diff(now, 'minutes') / 60) * 2
+    }
   },
   watch: {
     offsetMinutes (minutes, oldMinutes) {
@@ -234,6 +238,7 @@ export default {
       var holdUntil = moment(this.holdUntil)
 
       if(Math.abs(holdUntil.diff(time, 'minutes')) < 15 && oldMinutes !== 0) {
+        this.$emit('update:presetMode', 'HOLD_UNTIL')
         return
       }
 
@@ -260,9 +265,28 @@ export default {
     },
     hvacMode (mode) {
       this.requestedMode = mode
+    },
+    holdUntil (until) {
+      if(this.isHoldUntil) {
+        var holdUntil = moment(until)
+        var now = moment()
+
+        this.requestedTime = this.roundHalf(holdUntil.diff(now, 'minutes') / 60) * 2
+      } else {
+        this.requestedTime = 0
+      }
     }
   },
   computed: {
+    hasHoldUntil() {
+      return this.holdUntil.length > 0
+    },
+    isHeating () {
+      return this.hvacMode === 'heat'
+    },
+    isHoldUntil () {
+      return this.presetMode === 'HOLD_UNTIL'
+    },
     radius () {
       return this.diameter / 2
     },
@@ -280,11 +304,8 @@ export default {
         'pointer-events': this.mode === 'time' ? 'initial' : 'none'
       }
     },
-    heating () {
-      return this.hvacMode === 'heat'
-    },
     backgroundColor () {
-      if (this.heating) {
+      if (this.isHeating) {
         return this.heatingColor
       } else {
         return this.inactiveBackgroundColor
@@ -340,11 +361,8 @@ export default {
         })
       }
     },
-    isHoldUntil () {
-      return this.presetMode === 'HOLD_UNTIL'
-    },
     stateIconColor () {
-      if (this.heating) {
+      if (this.isHeating) {
         return this.activeTickColor
       } else {
         return this.inactiveTickColor
@@ -360,6 +378,17 @@ export default {
     displayHoldUntilAttribute () {
       return moment(this.holdUntil).locale('uk').format('LTS')
     },
+    iconOffset() {
+      var _this = this
+
+      return {
+        x: _this.hasHoldUntil ? _this.diameter * 0.36 : _this.diameter * 0.44,
+        y: _this.hasHoldUntil ? _this.diameter * 0.83 : _this.diameter * 0.85,
+        offsetX(x) {
+          return _this.diameter * (0.36 + x)
+        }
+      }
+    }
   },
   methods: {
     getAmbientLabel(position) {
@@ -383,7 +412,7 @@ export default {
     getActiveTrack(low, high) {
       return {
         fill: 'none',
-        stroke: this.heating ? this.heatingColor : this.inactiveTickColor,
+        stroke: this.isHeating ? this.heatingColor : this.inactiveTickColor,
         'stroke-linecap': 'round',
         'stroke-width': 20,
         d: describeArcPath(this.radius, this.radius, this.radius * 0.8, low, high),
@@ -425,12 +454,6 @@ export default {
       } else {
         this.mode = 'temp'
       }
-    },
-    iconOffsetX (x = 0) {
-      return this.diameter * (0.36 + x)
-    },
-    iconOffsetY (y = 0) {
-      return this.diameter * (0.83 + y)
     },
     roundHalf(num) {
 		  return Math.round (num * 2) /2
